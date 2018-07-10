fe1961d19248c42ef1df8ca9091455007df3da69
Savannac <savannavchada@gmail.com>
2018-07-10 21:37:17 +0000

    automatically backed up by learn

--- currency.rb
+++ currency.rb
@@ -7,3 +7,30 @@ def eur_to_usd(money)
   newmoney = money * 1.17 
   money
 end 
+
+def usd_to_jpy(money)
+  newmoney = money * 0.85 
+  money
+end 
+def jpy_to_usd(money)
+  newmoney = money * 1.17 
+  money
+end 
+
+def usd_to_aud(money)
+  newmoney = money * 0.85 
+  money
+end 
+def aud_to_usd(money)
+  newmoney = money * 1.17 
+  money
+end 
+
+def usd_to_gbp(money)
+  newmoney = money * 0.85 
+  money
+end 
+def gbp_to_usd(money)
+  newmoney = money * 1.17 
+  money
+end 
